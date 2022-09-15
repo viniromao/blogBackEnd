5747779a04386bffce59d7f7e85cc7c44cc4e250
eduga3 <edulindorodrigues@gmail.com>
2022-09-15 18:46:31 -0300

    Creating the Login Form skeleton

--- src/webapp/blog/src/App.js
+++ src/webapp/blog/src/App.js
@@ -1,9 +1,11 @@
 import FormRegister from "./components/FormRegister";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
     <div className="App">
       <FormRegister />
+      <LoginForm/>
     </div>
   );
 }
---
+++ src/webapp/blog/src/components/LoginForm/index.js
@@ -0,0 +1,40 @@
+import './Login.css'
+
+const LoginForm = () => {
+  return (
+    <div className='form-content-right'>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+      <form className='form'>
+          <div className='form-inputs'>
+            <label className='form-label'>Username</label>
+            <input
+              className='form-input'
+              type='text'
+              name='username'
+              placeholder='Enter your username'
+            />
+          </div>
+          <div className='form-inputs'>
+            <label className='form-label'>Password</label>
+            <input
+              className='form-input'
+              type='password'
+              name='password'
+              placeholder='Enter your password'
+            />
+          </div>
+          <button className='form-input-btn' type='submit'>
+            Login
+          </button>
+          <a href="">Forgot your password?</a>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm
