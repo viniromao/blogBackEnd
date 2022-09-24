c3e46b8b98b1eaf3b6b546d8faecfcfe11f6b7c4
eduga3 <edulindorodrigues@gmail.com>
2022-09-23 22:23:54 -0300

    .

---
+++ src/webapp/blog/src/components/Leftpanel/index.js
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom'
+import './Leftpanel.css'
+
+const Leftpanel = () => {
+    return (
+        <div className='Leftpanel'>
+            <div className='form-inputs_search'>
+                <img src='/imgs/grey_search_icon.png' />
+                <input
+                    className='form-input_search'
+                    type='text'
+                    name='search'
+                    placeholder='Search' />
+            </div>
+            <h2>MENU</h2>
+            <Link to='/'>
+                <button className='questions' type='submit'>
+                    Questions
+                </button>
+            </Link>
+        </div>
+    )
+}
+
+export default Leftpanel 
--- src/webapp/blog/src/components/Mainpage/index.js
+++ src/webapp/blog/src/components/Mainpage/index.js
@@ -1,29 +1,12 @@
 import './Mainpage.css'
-import { Link } from 'react-router-dom'
 import NavBar from '../NavBar'
+import Leftpanel from '../Leftpanel'
 
 const Mainpage = () => {
     return (
         <>
             <NavBar/>
-            <div className='Leftpanel'>
-                
-                <div className='form-inputs_search'>
-                    <img src='/imgs/grey_search_icon.png' />
-                    <input
-                        className='form-input_search'
-                        type='text'
-                        name='search'
-                        placeholder='Search' />
-                </div>
-
-                <h2>MENU</h2>
-                <Link to='/'>
-                    <button className='questions' type='submit'>
-                        Questions
-                    </button>
-                </Link>
-            </div>
+            <Leftpanel/>
         </>
     )
 }
