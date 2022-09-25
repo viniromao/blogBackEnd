143cd3073efe5a620e85cfc1f1371a3d5c810d44
eduga3 <edulindorodrigues@gmail.com>
2022-09-24 21:34:12 -0300

    left panel css incomplete

--- src/webapp/blog/src/components/Leftpanel/index.js
+++ src/webapp/blog/src/components/Leftpanel/index.js
@@ -5,7 +5,7 @@ const Leftpanel = () => {
     return (
         <div className='Leftpanel'>
             <div className='form-inputs_search'>
-                <img src='/imgs/grey_search_icon.png' />
+                <img src='/imgs/grey_search_icon.png' alt='searchIcon'/>
                 <input
                     className='form-input_search'
                     type='text'
@@ -13,6 +13,7 @@ const Leftpanel = () => {
                     placeholder='Search' />
             </div>
             <h2>MENU</h2>
+            <img src='imgs/listicon.png' alt='listIcon' className='listicon'/>
             <Link to='/'>
                 <button className='questions' type='submit'>
                     Questions
