b1a36d567e0b4358b73d465382d488904b39c170
eduga3 <edulindorodrigues@gmail.com>
2022-09-26 12:10:55 -0300

    Creating the SearchBar filter, css unfinished

---
+++ src/webapp/blog/src/components/SearchBar/index.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./SearchBar.css";
+
+function SearchBar({ placeholder, data }) {
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
+
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    setWordEntered(searchWord);
+    const newFilter = data.filter((value) => {
+      return value.title.toLowerCase().includes(searchWord.toLowerCase());
+    });
+
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered("");
+  };
+
+  return (
+    <div className="search">
+      <div className="searchInputs">
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={wordEntered}
+          onChange={handleFilter}
+        />
+        <div className="searchIcon">
+          {filteredData.length === 0 ? (
+            <img src="imgs/grey_search_icon.png" className="searchIconGrey"/>
+          ) : (
+            <img src="imgs/close.png" id="clearBtn" onClick={clearInput} className="searchIconGrey"/>
+          )}
+        </div>
+      </div>
+      {filteredData.length != 0 && (
+        <div className="dataResult">
+          {filteredData.slice(0, 15).map((value, key) => {
+            return (
+              <a className="dataName" href={value.link} target="_blank">
+                <p>{value.title} </p>
+              </a>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default SearchBar;
